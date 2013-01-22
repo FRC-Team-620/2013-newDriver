b2160c08a2d25fc906b633af57afec0c26746f7a
Team 620 <team620warbots@gmail.com>
2013-01-22 18:35:40 -0500

    Changed to proper template. Added message

--- newDriver/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ newDriver/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -8,67 +8,34 @@
 package edu.wpi.first.wpilibj.templates;
 
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.templates.commands.CommandBase;
-import edu.wpi.first.wpilibj.templates.commands.ExampleCommand;
+import edu.wpi.first.wpilibj.SimpleRobot;
 
 /**
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
+ * functions corresponding to each mode, as described in the SimpleRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class RobotTemplate extends IterativeRobot {
-
-    Command autonomousCommand;
-
+public class RobotTemplate extends SimpleRobot {
     /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
+     * This function is called once each time the robot enters autonomous mode.
      */
-    public void robotInit() {
-        // instantiate the command used for the autonomous period
-        autonomousCommand = new ExampleCommand();
-
-        // Initialize all subsystems
-        CommandBase.init();
-    }
-
-    public void autonomousInit() {
-        // schedule the autonomous command (example)
-        autonomousCommand.start();
+    public void autonomous() {
+        
     }
 
     /**
-     * This function is called periodically during autonomous
+     * This function is called once each time the robot enters operator control.
      */
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
+    public void operatorControl() {
 
-    public void teleopInit() {
-	// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        autonomousCommand.cancel();
-    }
-
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
     }
     
     /**
-     * This function is called periodically during test mode
+     * This function is called once each time the robot enters test mode.
      */
-    public void testPeriodic() {
-        LiveWindow.run();
+    public void test() {
+    
     }
 }
